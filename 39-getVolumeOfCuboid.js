4b4c78eaaf5be2da09c1d5fbcc97f1d78cbeaaff
FredoVz <dm.fredo018@gmail.com>
2026-07-03 09:07:05 +0700

    Day 39/366

---
+++ 39-getVolumeOfCuboid.js
@@ -0,0 +1,21 @@
+// Fredo Coding Challenge 2024
+// 39/366
+// Title = Volume of a Cuboid
+// By = user5196477
+// Level 8 Kyu
+// Tipe soal = Geometry, Fundamentals, & Mathematics
+// Source = https://www.codewars.com/kata/58261acb22be6e2ed800003a
+
+// Soal
+// class Kata {
+//   static getVolumeOfCuboid(length, width, height) {
+//     return length * width * height;
+//   }
+// }
+
+// Cara 1 - Normal Arrow Function
+const getVolumeOfCuboid1 = (l, w, h) => l * w * h;
+
+const getVolumeOfCuboid = getVolumeOfCuboid1;
+
+console.log(getVolumeOfCuboid(1, 2, 2));
