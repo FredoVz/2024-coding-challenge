95ca003b906db05f1480725fc2d6247cb3f38c30
FredoVz <dm.fredo018@gmail.com>
2026-07-17 07:27:34 +0700

    Day 2/366 (Kyu 7)

---
+++ 7 Kyu/2-descendingOrder.js
@@ -0,0 +1,20 @@
+// Fredo Coding Challenge 2023 (Live)
+// 2/366
+// Title = Descending Order
+// By = TastyOs
+// Level 7 Kyu
+// Tipe soal = Fundamentals
+// Source = https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+function descendingOrder(n) {
+  const nArr = n.toString().split("");
+  // [2,3,1]
+  return Number(nArr.sort((a, b) => b - a).join(""));
+}
+
+console.log(descendingOrder(0));
+console.log(descendingOrder(1), 1);
+console.log(descendingOrder(111), 111);
+console.log(descendingOrder(15), 51);
+console.log(descendingOrder(1021), 2110);
+console.log(descendingOrder(123456789), 987654321);
