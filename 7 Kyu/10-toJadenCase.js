f9b65c8acc64f9fc9f831b4bf8d3ff308ea5e156
FredoVz <dm.fredo018@gmail.com>
2026-07-17 22:48:18 +0700

    Day 10/366 (7 Kyu)

---
+++ 7 Kyu/10-toJadenCase.js
@@ -0,0 +1,16 @@
+// Fredo Coding Challenge 2023 (Live)
+// 10/366
+// Title = Jaden Casing Strings
+// By = jacobb
+// Level 7 Kyu
+// Tipe soal = Strings & Fundamentals
+// Source = https://www.codewars.com/kata/5390bac347d09b7da40006f6
+
+String.prototype.toJadenCase = function () {
+  this.split(" ")
+    .map((kata) => kata[0].toUpperCase() + kata.substring(1))
+    .join(" ");
+};
+
+const str = "How can mirrors be real if our eyes aren't real";
+console.log(str.toJadenCase());
