023649e04a137b5eeb2430f5e74463b711577204
FredoVz <dm.fredo018@gmail.com>
2026-07-17 08:40:19 +0700

    Day 2/366 (Kyu 7)

--- 7 Kyu/2-descendingOrder.js
+++ 7 Kyu/2-descendingOrder.js
@@ -6,11 +6,23 @@
 // Tipe soal = Fundamentals
 // Source = https://www.codewars.com/kata/5467e4d82edf8bbf40000155
 
-function descendingOrder(n) {
+// Cara 1 - toString, Split, Sort, Join
+function descendingOrder1(n) {
   const nArr = n.toString().split("");
   // [2,3,1]
   return Number(nArr.sort((a, b) => b - a).join(""));
 }
+
+const descendingOrder2 = (n) =>
+  Number(
+    n
+      .toString()
+      .split("")
+      .sort((a, b) => b - a)
+      .join(""),
+  );
+
+const descendingOrder = descendingOrder2;
 
 console.log(descendingOrder(0));
 console.log(descendingOrder(1), 1);
---
+++ 7 Kyu/3-accum.js
@@ -0,0 +1,14 @@
+// Fredo Coding Challenge 2023 (Live)
+// 3/366
+// Title = Mumbling
+// By = g964
+// Level 7 Kyu
+// Tipe soal = Fundamentals
+// Source = https://www.codewars.com/kata/5467e4d82edf8bbf40000155
+
+function accum(s) {
+  // your code
+  return "";
+}
+
+console.log(accum("ZpglnRxqenU"));
