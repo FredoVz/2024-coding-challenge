5bcae83ffe44823e23d42ae0ad91a761cbcdab76
FredoVz <dm.fredo018@gmail.com>
2026-07-18 10:57:46 +0700

    Day 11/366 (7 Kyu)

---
+++ 7 Kyu/11-dnaStrand.js
@@ -0,0 +1,63 @@
+// Fredo Coding Challenge 2023 (Live)
+// 10/366
+// Title = Complementary DNA
+// By = JustyFY
+// Level 7 Kyu
+// Tipe soal = Strings & Fundamentals
+// Source = https://www.codewars.com/kata/554e4a2f232cdd87d9000038
+
+// Cara 1 - For Loop If
+function dnaStrand1(dna) {
+  let dnaBaru = "";
+  for (let i = 0; i < dna.length; i++) {
+    if (dna[i] === "A") {
+      dnaBaru += "T";
+    } else if (dna[i] === "T") {
+      dnaBaru += "A";
+    } else if (dna[i] === "G") {
+      dnaBaru += "C";
+    } else if (dna[i] === "C") {
+      dnaBaru += "G";
+    }
+  }
+  return dnaBaru;
+}
+
+// Cara 2- For Loop (Switch Case)
+function dnaStrand2(dna) {
+  let dnaBaru = "";
+  for (let i = 0; i < dna.length; i++) {
+    switch (dna[i]) {
+      case "A":
+        dnaBaru += "T";
+        break;
+      case "T":
+        dnaBaru += "A";
+        break;
+      case "G":
+        dnaBaru += "C";
+        break;
+      case "C":
+        dnaBaru += "G";
+        break;
+      default:
+        break;
+    }
+  }
+  return dnaBaru;
+}
+
+// Cara 3 - For Loop (Ternary Operator)
+function dnaStrand3(dna) {
+  let dnaBaru = "";
+  for (let i = 0; i < dna.length; i++) {
+    dnaBaru += dna[i] === "A" ? "T" : dna[i] === "T" ? "A" : dna[i] === "G" ? "C" : "G";
+  }
+  return dnaBaru;
+}
+
+const dnaStrand = dnaStrand3;
+
+console.log(dnaStrand("AAAA"), "TTTT");
+console.log(dnaStrand("ATTGC"), "TAACG");
+console.log(dnaStrand("GTAT"), "CATA");
