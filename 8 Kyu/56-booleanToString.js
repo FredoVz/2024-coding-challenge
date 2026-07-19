1323024b3761f5833d5bb6ca6a4deedf63314820
FredoVz <dm.fredo018@gmail.com>
2026-07-19 10:54:59 +0700

    Day 56/366 (8 Kyu)

---
+++ 8 Kyu/56-booleanToString.js
@@ -0,0 +1,12 @@
+// Fredo Coding Challenge 2023 (Live)
+// 56/366
+// Title = Convert a Boolean to a String
+// By = btaitelb
+// Level 8 Kyu
+// Tipe soal = Strings & Fundamentals
+// Source = https://www.codewars.com/kata/551b4501ac0447318f0009cd
+
+// Cara 1
+function booleanToString(b) {}
+
+console.log(booleanToString(true));
