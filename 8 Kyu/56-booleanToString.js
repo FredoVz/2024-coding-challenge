86c6c8920c157efd75f516fdd407e00040bb0338
FredoVz <dm.fredo018@gmail.com>
2026-07-19 22:35:37 +0700

    Day 56/366 (8 Kyu)

--- 8 Kyu/56-booleanToString.js
+++ 8 Kyu/56-booleanToString.js
@@ -7,6 +7,20 @@
 // Source = https://www.codewars.com/kata/551b4501ac0447318f0009cd
 
 // Cara 1
-function booleanToString(b) {}
+function booleanToString1(b) {
+  return b ? "true" : "false";
+}
+
+// Cara 2 - Arrow Function
+const booleanToString2 = (b) => (b ? "true" : "false");
+
+// Cara 3 - toString
+const booleanToString3 = (b) => b.toString();
+
+// Cara 4 - Template Literal
+const booleanToString4 = (b) => `${b}`;
+
+const booleanToString = booleanToString4;
 
 console.log(booleanToString(true));
+console.log(booleanToString(false));
